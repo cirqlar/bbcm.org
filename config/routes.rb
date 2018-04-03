448ef7fa499578da21444f1dae94ac5d484cb22a
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2018-04-03 19:27:18 +0100

    Edit image routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'images/index'
 
-  get 'images/show'
+  #Gallery routes
+  get 'gallery', to: 'images#index'
+  get 'gallery/:id', to: 'images#show', as: 'image'
+  authenticate :admin do
+    resources :images, except: [:index, :show]
+  end
 
-  get 'images/edit'
-
-  get 'images/new'
-
+  # Admin routes
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
