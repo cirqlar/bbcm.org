84732850685b83b1371ed8230bd5bed265eb9697
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2018-04-07 13:36:30 +0100

    Temp Commit

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,13 +1,52 @@
 class NewsController < ApplicationController
+  before_action :get_news, only: [:show, :edit, :update, :destroy]
+
   def index
+    @news_p = News.all
   end
 
   def show
   end
 
+  def new
+    @news = News.new
+  end
+
+  def create
+    @news = News.new(news_params)
+    if @news.save!
+      redirect_to @news
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
-  def new
+  def update
+    if @news.update_attributes(news_params)
+      redirect_to @news
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    if @news.destroy
+      redirect_to news_index_path
+    else
+      redirect_to @news
+    end
+  end
+
+  private
+
+    def get_news
+      @news = News.find_by(params[:id])
+    end
+
+    def news_params
+      params.require(:news).permit(:title, :message)
+    end
 end
